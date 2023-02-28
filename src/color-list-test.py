5cc37fb5933aa0c8fcd63e4f22229a8fc7765fe7
dmccreary <dan.mccreary@gmail.com>
2023-02-27 23:31:04 -0600

    updated content

--- src/color-list-test.py
+++ src/color-list-test.py
@@ -14,23 +14,28 @@ ON = 1
 OFF = 0
 NO_FILL = 0
 FILL = 1
+# bit order is blue, red, green
 black = 0x0000
 red   = 0x07E0
 green = 0x001f
 blue  = 0xf800
 white = 0xffff
-yellow = 0xEFE4
-orange = 0xFFA500
-cyan = 0x00FFFF
-brown = 0xA52A2A
-gold = 0xFFD700
-purple = 0x800080
-pink = 0x07E0
-gray = 0x808080
-lightGreen = 0x90EE90
+#
+yellow = 0b0000011111111111
+orange = 0b0000001111000010
+cyan = 0b1111100000111111
+brown = 0b0000000001000001
+gold = 0b0000001110011110
+purple = 0b1111111111000000
+magenta = 0b0000001100011000
+pink = 0b0000111111000010
+olive = 0b0000000001000010
+gray = 0b00001000010000100
+lightGreen = 0b0000100001111111
+darkGreen  = 0b0000000000000001
 
-ColorList = (red, green, blue, white, yellow, orange, cyan, brown, gold, purple, pink, gray, lightGreen)
-ColorNames = ('red', 'green', 'blue', 'white', 'yellow', 'orange', 'cyan', 'brown', 'gold', 'purple', 'pink', 'gray', 'lightGreen')
+ColorList = (red, green, blue, white, yellow, orange, cyan, brown, gold, purple, magenta, pink, olive, gray, lightGreen, darkGreen)
+ColorNames = ('red', 'green', 'blue', 'white', 'yellow', 'orange', 'cyan', 'brown', 'gold', 'purple', 'magenta', 'pink', 'olive', 'gray', 'lightGreen', 'darkGreen')
 
 radius = 120
 LCD.fill(LCD.black)
@@ -39,5 +44,6 @@ for i in range(0, len(ColorList)):
     print(ColorNames[i])
     LCD.ellipse(CENTER, CENTER, radius, radius, ColorList[i], FILL)
     LCD.text(ColorNames[i],100,100,1)
+    LCD.text(ColorNames[i],100,120,white)
     LCD.show()
-    sleep(3)
+    sleep(2)
