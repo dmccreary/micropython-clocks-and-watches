976eec84d7c2b2465fa7f3e1fc2a7fe67ab79220
Dan Mccreary <Dan.McCreary@gmail.com>
2023-10-19 16:35:36 -0500

    updating content

--- src/kits/pico-w-oled/config.py
+++ src/kits/pico-w-oled/config.py
@@ -1,2 +1,20 @@
-wifi_ssid = 'anndan-4.2'
-wifi_pass = 'stargate123'
+# Dan's Robot Labs configuration file for Raspberry Pi Pico and an SPI OLED Display
+# The colors on the SPI bus cable are:
+# 3.3v power - red
+# SCK - orange
+# MISO/Data - yellow
+# DC - green
+# RESET - blue
+# GND - black
+# CS - purple
+
+SPI_BUS = 0
+SCL_PIN = 2 # Clock
+SDA_PIN = 3 # labeled SDI(MOSI) on the back of the display
+RESET_PIN = 4 # Reset
+DC_PIN = 5 # Data/command
+CS_PIN = 6 # Chip Select
+
+# OLED Screen Dimensions
+WIDTH=128
+HEIGHT=64
